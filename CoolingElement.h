f45dcf9ccd79c4b03902ec204cc1fd6ba7494df6
Buitendijk,Emma E.M.C <e.buitendijk@student.fontys.nl>
2017-05-29 13:35:30 +0200

    File opzet

---
+++ CoolingElement.h
@@ -0,0 +1,11 @@
+#ifndef COOLINGELEMENT_H
+#define COOLINGELEMENT_H
+class CoolingElement
+{
+public:
+  void On(void);
+  void Off(void);
+private:
+};
+
+#endif
---
+++ TempSensor.h
@@ -0,0 +1,11 @@
+#ifndef TEMPSENSOR_H
+#define TEMPSENSOR_H
+class TempSensor
+{
+public:
+  int GetTemperatureValue(void);
+private:
+
+};
+
+#endif
